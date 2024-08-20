b8c1a8835b100cdca76a5d710d7f708b3d2d2c87
Shawn Osborne <shawnby222@gmail.com>
2024-08-20 15:54:48 -0500

    adds dynamic imports of country SVGs when routed to.

--- Interactive_World_Map/src/components/SelectionOutput.vue
+++ Interactive_World_Map/src/components/SelectionOutput.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+
 //prop sent through route url (/country/countryId)
 const props = defineProps({
   countryId: {
@@ -7,12 +9,22 @@ const props = defineProps({
   }
 });
 
+const countrySvg = ref('');
+
+async function getSvg(countryId) {
+  let svg = await import(`../assets/svg_countries/${countryId}.svg?raw`);
+  countrySvg.value = svg.default;
+}
+
+onMounted(() => {
+  getSvg(props.countryId);
+})
+
 </script>
 
 <template>
   <RouterLink to="/">Back To Map</RouterLink>
-  <div class="map-image">
-    {{ $route.params.countryId }}
+  <div class="map-image" v-html="countrySvg">
   </div>
 </template>
 
