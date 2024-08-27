9af69db1b0387b742a42192a813f882fe850b762
Shawn Osborne <shawnby222@gmail.com>
2024-08-27 16:00:03 -0500

    Adds map controls

--- Interactive_World_Map/src/components/Map.vue
+++ Interactive_World_Map/src/components/Map.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, reactive, ref, shallowRef, watch } from 'vue';
+import { onMounted, reactive, ref, shallowRef } from 'vue';
 import { gsap } from 'gsap/gsap-core';
 
 //next add manual zoom and pan using viewBox and +- <> buttons
@@ -11,22 +11,27 @@ const defaultViewBox = {
     width: 1010,
     height: 665
 };
+const currentViewBox = { ...defaultViewBox };
 const tooltip = reactive({
     text: '',
     top: 0,
     left: 0
 });
+
 const mapImage = shallowRef('');
-
+const zoomed = ref(false);
+const countryView = ref(false);
 let mapElement;
-
-let zoomed = ref(false);
 
 async function loadMap() {
     //import svg as component
     let map = await import('../assets/svgs/map-image.svg?component');
     mapImage.value = map.default;
 };
+
+function setSelector() {
+    mapElement = document.getElementById('world-map')
+}
 
 //display tooltip of country name while changing position with mouse cursor
 function displayTip(event) {
@@ -57,6 +62,12 @@ function displayTip(event) {
     }
 };
 
+function updateViewBox(delayAmt) {
+    const vbString = `${currentViewBox.x} ${currentViewBox.y} ${currentViewBox.width} ${currentViewBox.height}`;
+    let moveTween = gsap.to('#world-map', { delay: delayAmt, duration: 0.2, attr: { viewBox: vbString }, ease: "power1.in" });
+    moveTween.play();
+}
+
 function handleSvgClick(event) {
     const element = event.target;
     //console.log(element)
@@ -71,21 +82,27 @@ function handleSvgClick(event) {
             newVb.x -= 40
         }
         if (newVb.width < defaultViewBox.width) {
-            const vbString = `${newVb.x} ${newVb.y} ${newVb.width} ${newVb.height}`;
-            let moveTween = gsap.to('.world-map', { delay: 0.2, duration: 0.2, attr: { viewBox: vbString }, ease: "power1.in" });
-            moveTween.play();
+            currentViewBox.width = newVb.width;
+            currentViewBox.height = newVb.height
+            currentViewBox.x = newVb.x;
+            currentViewBox.y = newVb.y;
+            updateViewBox(0.2)
         }
         zoomed.value = true;
+        countryView.value = true;
         emit('countrySvgId', elId);
     }
 };
 
 function zoomOutFully() {
     toggleSelection()
-    const vbString = `${defaultViewBox.x} ${defaultViewBox.y} ${defaultViewBox.width} ${defaultViewBox.height}`;
-    let tween = gsap.to('.world-map', { duration: 0.2, attr: { viewBox: vbString }, ease: "power1.in" });
-    tween.play();
+    currentViewBox.x = defaultViewBox.x;
+    currentViewBox.y = defaultViewBox.y;
+    currentViewBox.width = defaultViewBox.width;
+    currentViewBox.height = defaultViewBox.height;
     zoomed.value = false;
+    countryView.value = false;
+    updateViewBox(0)
 };
 
 function toggleSelection(elem = '') {
@@ -102,20 +119,41 @@ function toggleSelection(elem = '') {
     });
 };
 
-function zoomIn() {
+function zoomMap(dir) {
+    //map control - zoom
+    const zoomFactor = dir == "in" ? 0.75 : 1.25;
+    const centerX = currentViewBox.x + currentViewBox.width / 2;
+    const centerY = currentViewBox.y + currentViewBox.height / 2;
+    currentViewBox.width *= zoomFactor;
+    currentViewBox.height *= zoomFactor;
+    currentViewBox.x = centerX - currentViewBox.width / 2;
+    currentViewBox.y = centerY - currentViewBox.height / 2;
+    if (defaultViewBox.width != currentViewBox.width) {
+        zoomed.value = true;
+    } else {
+        zoomed.value = false;
+    }
+    updateViewBox(0);
 
 };
 
-function zoomOut() {
-
-};
-
-function panLeft() {
-
-};
-
-function panRight() {
-
+function panMap(dir) {
+    //map control - pan
+    switch (dir) {
+        case "up":
+            currentViewBox.y -= 25
+            break;
+        case "down":
+            currentViewBox.y += 25
+            break;
+        case "left":
+            currentViewBox.x -= 25
+            break;
+        case "right":
+            currentViewBox.x += 25
+            break;
+    }
+    updateViewBox(0)
 };
 
 onMounted(() => {
@@ -126,18 +164,20 @@ onMounted(() => {
 
 <template>
     <!-- Handles display of Map SVG - defines click events and mousemove/hover events for tooltip reaction -->
-    <RouterLink to="/" v-if="zoomed" @click="zoomOutFully" class="link-btn">World Map</RouterLink>
-
-    <div class="map-controls-container" v-else>
-        <p id="map-controls-head">Map Controls</p>
-        <button class="map-control" id="zoom-in">+</button>
-        <button class="map-control" id="zoom-out">-</button>
-        <button class="map-control" id="pan-left"><</button>
-        <button class="map-control" id="pan-right">></button>
+    <div class="map-control-container">
+        <div class="map-controls" v-if="!countryView">
+            <p id="map-controls-head">Map Controls</p>
+            <button class="map-control" id="pan-up" @click="panMap('up')">&uarr;</button>
+            <button class="map-control" id="pan-down" @click="panMap('down')">&darr;</button>
+            <button class="map-control" id="pan-left" @click="panMap('left')">&larr;</button>
+            <button class="map-control" id="pan-right" @click="panMap('right')">&rarr;</button>
+            <button class="map-control" id="zoom-in" @click="zoomMap('in')">+</button>
+            <button class="map-control" id="zoom-out" @click="zoomMap('out')">-</button>
+        </div>
+        <button class="map-control" to="/" v-if="zoomed" @click="zoomOutFully">Default Map</button>
     </div>
-
     <div class="map-image" @click="handleSvgClick" @mousemove="displayTip" v-if="mapImage">
-        <mapImage></mapImage>
+        <mapImage @vue:mounted="setSelector"></mapImage>
     </div>
 
     <div v-else>
@@ -150,35 +190,67 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.map-controls-container {
+.map-control-container {
     position: absolute;
     z-index: 5;
     top: 0.5rem;
     left: 0.5rem;
+    display: flex;
+    flex-direction: column;
+}
+
+.map-controls {
     display: grid;
-    gap: 0.25rem;
     grid-template-areas:
         "head head head"
-        ". z-in ."
+        ". p-up ."
         "p-left . p-right"
-        ". z-out .";
+        ". p-down ."
+        "z-in . z-out";
+    grid-template-columns: repeat(3, 1fr);
     background-color: var(--color-sidebar-bg);
     color: var(--color-text);
-    padding: 1rem;
+    padding: 0.75rem;
+    border-radius: 0.25rem;
+}
+
+.map-control {
     border-radius: 0.5rem;
+    padding: 0.25rem;
+    border: none;
+    cursor: pointer;
+}
+
+.map-control:hover {
+    background-color: var(--color-sidebar-bg);
+    color: var(--color-text);
+}
+
+.map-control:active {
+    background-color: var(--color-tooltip-bg);
 }
 
 #map-controls-head {
     grid-area: head;
     padding-bottom: 1rem;
+    justify-self: center;
 }
 
+/* grid-area-name grid-row-start / grid-column-start / grid-row-end / grid-column-end */
 #zoom-in {
     grid-area: z-in;
 }
 
 #zoom-out {
     grid-area: z-out;
+}
+
+#pan-up {
+    grid-area: p-up;
+}
+
+#pan-down {
+    grid-area: p-down;
 }
 
 #pan-left {
@@ -201,10 +273,5 @@ onMounted(() => {
     white-space: nowrap;
 }
 
-.link-btn {
-    position: absolute;
-    z-index: 5;
-    top: 1rem;
-    left: 1rem;
-}
+.link-btn {}
 </style>
