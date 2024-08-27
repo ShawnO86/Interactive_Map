035864d3a0727a07e829322a65237acf459491ca
Shawn Osborne <shawnby222@gmail.com>
2024-08-27 12:57:14 -0500

    Adds map controls

--- Interactive_World_Map/src/components/Map.vue
+++ Interactive_World_Map/src/components/Map.vue
@@ -3,6 +3,8 @@ import { onMounted, reactive, ref, shallowRef } from 'vue';
 import { gsap } from 'gsap/gsap-core';
 
 
+//next add manual zoom and pan using viewBox and +- <> buttons
+
 const emit = defineEmits(['countrySvgId', 'zoomedOut']);
 const defaultViewBox = {
     x: 0,
@@ -18,10 +20,9 @@ const tooltip = reactive({
 const mapImage = shallowRef('');
 
 let zoomed = ref(false);
-let mapElement = ref('');
 
 async function loadMap() {
-    //import svg as raw string to use in v-html tag
+    //import svg as component
     let map = await import('../assets/svgs/map-image.svg?component');
     mapImage.value = map.default;
 };
@@ -47,31 +48,29 @@ function displayTip(event) {
             if (top < 10) {
                 top = 10;
             }
-            tooltip.top = top
-            tooltip.left = left - ttWidth
+            tooltip.top = top;
+            tooltip.left = left - ttWidth;
         }
     } else {
         tooltip.text = '';
     }
 };
 
-//emit the countrySvgId to App.vue to use with router to send to SelectionOutput.vue (/country/id)
 function handleSvgClick(event) {
     const element = event.target;
     //console.log(element)
     if (element.tagName == 'path') {
         const elId = element.getAttribute('id');
         const newVb = element.getBBox();
-        removeSelection(element)
-        element.classList.add('selected')
-        console.log("before:", newVb.width)
+        toggleSelection(element);
+        element.classList.add('selected');
+        //adds 80px to zoomed country final viewBox width
         if (newVb.width < 500) {
             newVb.width += 80
             newVb.x -= 40
         }
-        console.log("after:", newVb.width)
         if (newVb.width < defaultViewBox.width) {
-            const vbString = `${newVb.x} ${newVb.y} ${newVb.width} ${newVb.height}`
+            const vbString = `${newVb.x} ${newVb.y} ${newVb.width} ${newVb.height}`;
             let moveTween = gsap.to('.world-map', { delay: 0.2, duration: 0.2, attr: { viewBox: vbString }, ease: "power1.in" });
             moveTween.play();
         }
@@ -80,43 +79,50 @@ function handleSvgClick(event) {
     }
 };
 
-function zoomOut() {
-    removeSelection()
-    const vbString = `${defaultViewBox.x} ${defaultViewBox.y} ${defaultViewBox.width} ${defaultViewBox.height}`
+function zoomOutFully() {
+    toggleSelection()
+    const vbString = `${defaultViewBox.x} ${defaultViewBox.y} ${defaultViewBox.width} ${defaultViewBox.height}`;
     let tween = gsap.to('.world-map', { duration: 0.2, attr: { viewBox: vbString }, ease: "power1.in" });
     tween.play();
     zoomed.value = false;
 }
 
-function removeSelection(elem = '') {
-    const mapElement = document.querySelector('.world-map')
-    mapElement.childNodes.forEach(node => {
-        node.classList.remove('selected')
-        if (elem) {
-            if (node != elem) {
-                node.classList.add('invis')
-            }
+function toggleSelection(elem = '') {
+    const mapElementPaths = document.querySelector('.world-map').childNodes;
+    mapElementPaths.forEach(node => {
+        node.classList.remove('selected');
+        if (elem && node != elem) {
+            node.classList.add('invis');
         } else {
             setTimeout(() => {
-                node.classList.remove('invis')
-            }, 200)
+                node.classList.remove('invis');
+            }, 200);
         }
     });
 }
 
 onMounted(() => {
     loadMap();
-})
+});
 
 </script>
 
 <template>
     <!-- Handles display of Map SVG - defines click events and mousemove/hover events for tooltip reaction -->
-    <RouterLink to="/" v-if="zoomed" @click="zoomOut" class="link-btn">World Map</RouterLink>
+    <RouterLink to="/" v-if="zoomed" @click="zoomOutFully" class="link-btn">World Map</RouterLink>
+
+    <div class="map-controls-container" v-else>
+        <p id="map-controls-head">Map Controls</p>
+        <button class="map-control" id="zoom-in">+</button>
+        <button class="map-control" id="zoom-out">-</button>
+        <button class="map-control" id="pan-left"><</button>
+        <button class="map-control" id="pan-right">></button>
+    </div>
 
     <div class="map-image" @click="handleSvgClick" @mousemove="displayTip" v-if="mapImage">
         <mapImage></mapImage>
     </div>
+
     <div v-else>
         <p>Loading World Map Image...</p>
     </div>
@@ -127,6 +133,45 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.map-controls-container {
+    position: absolute;
+    z-index: 5;
+    top: 0.5rem;
+    left: 0.5rem;
+    display: grid;
+    gap: 0.25rem;
+    grid-template-areas:
+        "head head head"
+        ". z-in ."
+        "p-left . p-right"
+        ". z-out .";
+    background-color: var(--color-sidebar-bg);
+    color: var(--color-text);
+    padding: 1rem;
+    border-radius: 0.5rem;
+}
+
+#map-controls-head {
+    grid-area: head;
+    padding-bottom: 1rem;
+}
+
+#zoom-in {
+    grid-area: z-in;
+}
+
+#zoom-out {
+    grid-area: z-out;
+}
+
+#pan-left {
+    grid-area: p-left;
+}
+
+#pan-right {
+    grid-area: p-right;
+}
+
 .tooltip {
     position: absolute;
     z-index: 5;
