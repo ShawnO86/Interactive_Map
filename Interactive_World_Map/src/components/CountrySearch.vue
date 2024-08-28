d1f93f1b0bbb35733a488efb59d5f523b765df4c
Shawn Osborne <shawnby222@gmail.com>
2024-08-27 21:57:36 -0500

    Adds todos for tomorrow

--- Interactive_World_Map/src/components/CountrySearch.vue
+++ Interactive_World_Map/src/components/CountrySearch.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script setup>
-//Send search to selectionOutput.vue and map.vue using pinia???? 
-//Using search data, zoom to the country location and call API for country data???
+
+//allow for basic country search - (highlight searched country and display info(Capital City, Region, Income Level))
+//ex Brasil - http://api.worldbank.org/v2/country/br?format=json 
+//ex all indicator - https://api.worldbank.org/v2/country/all/indicator/1.1_ACCESS.ELECTRICITY.TOT?date=2023&format=xml
 </script>
 
 <style scoped></style>
